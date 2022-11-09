51f344db0eb1ea3c3b04c2a91f339f655ff95f3c
Arvin Lorenz Pineda <arvinlorenz@gmail.com>
2022-11-09 09:45:08 +0800

    change password

--- routes/users.js
+++ routes/users.js
@@ -80,7 +80,7 @@ router.post('/login', function (req, res, next) {
       connection.query(
         'select * from students where replace(lrn, "-", "") = ?',
         [newusername],
-        (err, result) => {
+        async (err, result) => {
           if (err) {
             console.log(err)
             res.status(201).json({
@@ -94,9 +94,15 @@ router.post('/login', function (req, res, next) {
               // employeeLogin(username, password)
             } else {
               // var hashedPassword = result[0].password
-              var passed =
-                password.trim().toLowerCase() ===
-                result[0].last_name.trim().toLowerCase()
+              var passed = false
+              if (!result[0].isPasswordChanged) {
+                passed =
+                  password.trim().toLowerCase() ===
+                  result[0].last_name.trim().toLowerCase()
+              }
+              if (result[0].isPasswordChanged) {
+                passed = await bcrypt.compare(password, result[0].password)
+              }
 
               if (passed == false) {
                 res.status(400).json({
@@ -217,6 +223,141 @@ router.post('/login', function (req, res, next) {
   //   }
 })
 
+router.post('/change-password', function (req, res, next) {
+  var oldPassword = req.body.oldPassword
+  var newPassword = req.body.newPassword
+  var username = req.body.username
+  var userType = req.body.userType
+  console.log(oldPassword, newPassword, username, userType)
+  if (userType === 'parent') {
+    mysql_pool.getConnection(function (err, connection) {
+      if (err) {
+        console.log(' Error getting mysql_pool connection: ' + err)
+        throw err
+      }
+      connection.query(
+        'select * from students where replace(lrn, "-", "") = ?',
+        [username],
+        async (err, result) => {
+          if (err) {
+            console.log(err)
+            res.status(404).json({
+              message: 'Not Found',
+            })
+          } else {
+            if (result.length == 0) {
+              res.status(400).json({
+                message: 'Auth failed',
+              })
+            } else {
+              if (!result[0].isPasswordChanged) {
+                // if password is never changed
+                var passed =
+                  oldPassword.trim().toLowerCase() ===
+                  result[0].last_name.trim().toLowerCase()
+                if (!passed) {
+                  res.status(201).json({
+                    message: 'You entered an incorrect password',
+                  })
+                }
+                if (passed) {
+                  var hashedPassword = await bcrypt.hash(newPassword, 10)
+                  connection.query(
+                    'UPDATE students SET isPasswordChanged = ?, password = ? where replace(lrn, "-", "") = ?',
+                    [1, hashedPassword, username],
+                    function (err, result) {
+                      if (err) throw err
+                    }
+                  )
+
+                  res.status(200).json({
+                    message: 'Password successfully updated!',
+                  })
+                }
+              }
+
+              if (result[0].isPasswordChanged) {
+                var passed = await bcrypt.compare(
+                  oldPassword,
+                  result[0].password
+                )
+                console.log(passed)
+                if (passed) {
+                  var hashedPassword = await bcrypt.hash(newPassword, 10)
+                  connection.query(
+                    'UPDATE students SET password = ? where replace(lrn, "-", "") = ?',
+                    [hashedPassword, username],
+                    function (err, result) {
+                      if (err) throw err
+                    }
+                  )
+
+                  res.status(200).json({
+                    message: 'Password successfully updated!',
+                  })
+                } else {
+                  res.status(201).json({
+                    message: 'You entered an incorrect password',
+                  })
+                }
+              }
+            }
+          }
+        }
+      )
+      connection.release()
+    })
+  }
+
+  if (userType === 'admin') {
+    mysql_pool.getConnection(function (err, connection) {
+      if (err) {
+        console.log(' Error getting mysql_pool connection: ' + err)
+        throw err
+      }
+      connection.query(
+        'select * from users where username = ?',
+        [username],
+        async (err, result) => {
+          if (err) {
+            console.log(err)
+            res.status(404).json({
+              message: 'Not Found',
+            })
+          } else {
+            if (result.length == 0) {
+              res.status(400).json({
+                message: 'Auth failed',
+              })
+            } else {
+              var passed = await bcrypt.compare(oldPassword, result[0].password)
+              console.log(passed)
+              if (passed) {
+                var hashedPassword = await bcrypt.hash(newPassword, 10)
+                connection.query(
+                  'UPDATE users SET password = ? where username = ?',
+                  [hashedPassword, username],
+                  function (err, result) {
+                    if (err) throw err
+                  }
+                )
+
+                res.status(200).json({
+                  message: 'Password successfully updated!',
+                })
+              } else {
+                res.status(201).json({
+                  message: 'You entered an incorrect password',
+                })
+              }
+            }
+          }
+        }
+      )
+      connection.release()
+    })
+  }
+})
 router.get('/logout', function (req, res, next) {
   res.redirect('/')
 })
