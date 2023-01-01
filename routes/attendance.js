45b9b9e1d0b95e130b10babdb788b94bf8d386d9
Arvin Lorenz Pineda <arvinlorenz@gmail.com>
2023-01-01 13:22:52 +0800

    attendance

--- routes/attendance.js
+++ routes/attendance.js
@@ -61,6 +61,9 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
   console.log('HOORAY', req.body)
+  res.status(200).json({
+    message: 'attendance was successfully created',
+  })
   // var recipients = req.body.recipients
   // var datetime = req.body.datetime
   // var status = req.body.status
