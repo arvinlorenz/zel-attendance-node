9ba7d0b84fd37379c5893b8059d3043e5694eb0b
Arvin Lorenz Pineda <arvinlorenz@gmail.com>
2023-01-23 22:04:35 +0800

    static page

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const userRoutes = require('./routes/users')
 const departmentRoutes = require('./routes/departments')
 const attendanceRoutes = require('./routes/attendance')
 const announcementRoutes = require('./routes/announcements')
+const pagesRoutes = require('./routes/pages')
 const app = express()
 const https = require('https')
 const fs = require('fs')
+const path = require('path')
+
 const privateKey = fs.readFileSync('zel-zams_com.key')
 const certificate = fs.readFileSync('zel-zams_com.crt')
 
 app.use(express.json())
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.use((req, res, next) => {
   res.setHeader('X-Auth-Token', '*')
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -30,6 +33,11 @@ app.use('/api/users', userRoutes)
 app.use('/api/departments', departmentRoutes)
 app.use('/api/attendance', attendanceRoutes)
 app.use('/api/announcements', announcementRoutes)
+app.use(pagesRoutes)
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
 var server = https
   .createServer(
     {
